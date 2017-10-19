1835438c0e4eaa13f9ff30f8e6924b9e399d8eca
DinoStark <15190304050@163.com>
2017-10-20 00:19:35 +0800

    Last update : 2017.10.20 00:19

---
+++ NetworkConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkConsole
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            WebRequestGetExample.RequestGetExample();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
---
+++ NetworkConsole/WebRequestGetExample.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace NetworkConsole
+{
+    /// <summary>
+    /// The WebRequestGetExample class provides an example for request web page content using a WebRequest.
+    /// </summary>
+    public class WebRequestGetExample
+    {
+        /// <summary>
+        /// This method provides an example for request web page content using a WebRequest.
+        /// </summary>
+        public static void RequestGetExample()
+        {
+            // Create a request for the URL.
+            WebRequest request = WebRequest.Create("http://www.contoso.com/default.html");
+
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+        }
+    }
+}
