00635cff165783e0a6a2341088e33be8cb2def3e
鲍方舟 <shakuganno00@163.com>
2017-11-13 21:57:11 +0800

    Last updated : 2017.11.13 21:57

--- ConcurrentProgrammingDemo/Program.cs
+++ ConcurrentProgrammingDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConcurrentProgrammingDemo
 {
@@ -12,12 +13,23 @@ namespace ConcurrentProgrammingDemo
         {
             //CreateThreadsAndPassData.CreateAThread();
             //CreateThreadsAndPassData.PassDataToThreads();
-            CreateThreadsAndPassData.RetriveDataFromThreads();
+            //CreateThreadsAndPassData.RetriveDataFromThreads();
+
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintNumbers();
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             return 0;
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
         }
     }
 }
