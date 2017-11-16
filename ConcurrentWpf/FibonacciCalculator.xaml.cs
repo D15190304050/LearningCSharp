603f85fc2cf64203c8638d204f726a51e7e1a415
鲍方舟 <shakuganno00@163.com>
2017-11-16 08:15:09 +0800

    Last updated : 2017.11.16 08:11

---
+++ ConcurrentWpf/FibonacciCalculator.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace ConcurrentWpf
+{
+    /// <summary>
+    /// FibonacciCalculator.xaml 的交互逻辑
+    /// </summary>
+    public partial class FibonacciCalculator : Window
+    {
+        private BackgroundWorker worker;
+        private int numberToCompute;
+        private int highestPercentageReached;
+
+        public FibonacciCalculator()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+            numberToCompute = 0;
+            highestPercentageReached = 0;
+        }
+
+        /// <summary>
+        /// Set up the BackgroundWorker object by attaching event handlers.
+        /// </summary>
+        private void InitializeBackgroundWorker()
+        {
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.ProgressChanged += worker_ProgressChanged;
+        }
+
+        private void cmdStartAsync_Click(object sender, RoutedEventArgs e)
+        {
+
+            txtResult.Text = "";
+            txtIndex.IsEnabled = false;
+            cmdCancelAsync.IsEnabled = true;
+            cmdStartAsync.IsEnabled = false;
+
+            numberToCompute = int.Parse(txtIndex.Text);
+            highestPercentageReached = 0;
+            worker.RunWorkerAsync(numberToCompute);
+        }
+
+        private void cmdCancelAsync_Click(object sender, RoutedEventArgs e)
+        {
+            worker.CancelAsync();
+            cmdCancelAsync.IsEnabled = false;
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (sender is BackgroundWorker worker)
+            {
+                e.Result = ComputeFibonacci((int)e.Argument, worker, e);
+            }
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+                MessageBox.Show(e.Error.Message);
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private long ComputeFibonacci(int n, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            return 0;
+        }
+
+
+    }
+}
---
+++ ConcurrentWpf/ProgressPercentage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace ConcurrentWpf
+{
+    /// <summary>
+    /// ProgressPercentage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProgressPercentage : Window
+    {
+        BackgroundWorker worker;
+
+        public ProgressPercentage()
+        {
+            InitializeComponent();
+            worker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true,
+            };
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+        }
+
+        private void cmdStart_Click(object sender, RoutedEventArgs e)
+        {
+            // Start the asynchronous operation if the BackgroundWorker is not busy.
+            if (!worker.IsBusy)
+                worker.RunWorkerAsync();
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Cancel the asynchronous operation if it supports cancellation.
+            if (worker.WorkerSupportsCancellation)
+                worker.CancelAsync();
+        }
+
+        /// <summary>
+        /// This event handler is where the time-consuming work is done.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (sender is BackgroundWorker worker)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (worker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    else
+                    {
+                        // Perform a time consuming operation and report progress.
+                        System.Threading.Thread.Sleep(500);
+                        worker.ReportProgress(i * 10);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This event handler updates the progress.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            txtProgress.Text = e.ProgressPercentage + "%";
+        }
+
+        /// <summary>
+        /// This event handler deal with the results of the background operation.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                txtProgress.Text = "Canceled!";
+            else if (e.Error != null)
+                txtProgress.Text = "Error: " + e.Error.Message;
+            else
+                txtProgress.Text = "Done";
+        }
+    }
+}
