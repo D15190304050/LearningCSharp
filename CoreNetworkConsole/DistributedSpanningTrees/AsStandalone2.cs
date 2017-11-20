4b4426f036adb69ed21ef475c26049bfe19b9636
鲍方舟 <shakuganno00@163.com>
2017-11-20 21:58:55 +0800

    Last updated : 2017.11.20 21:58

---
+++ CoreNetworkConsole/DistributedSpanningTrees/AsStandalone2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreNetworkConsole.DistributedSpanningTrees
+{
+    public class AsStandalone2
+    {
+        private Lan[] lans;
+
+        private Bridge2[] bridges;
+
+        private LinkedList<LanBridgeConnection> connections;
+
+        public AsStandalone2(int numLans, int numBridges)
+        {
+            lans = new Lan[numLans];
+            bridges = new Bridge2[numBridges];
+
+            for (int i = 0; i < numLans; i++)
+                lans[i] = new Lan(i + 1);
+
+            for (int i = 0; i < numBridges; i++)
+                bridges[i] = new Bridge2(i + 1);
+
+            connections = new LinkedList<LanBridgeConnection>();
+        }
+
+
+    }
+}
---
+++ CoreNetworkConsole/DistributedSpanningTrees/Lan.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CoreNetworkConsole.DistributedSpanningTrees
+{
+    public class Lan
+    {
+        public int Id { get; private set; }
+
+        public Bridge2 DesignateBridge { get; set; }
+
+        private LinkedList<LanBridgeConnection> connections;
+
+        public IEnumerable<LanBridgeConnection> Connections { get { return connections; } }
+
+        private LinkedList<Socket> sockets;
+
+        private Socket lanSocket;
+
+        public Lan(int id)
+        {
+            this.Id = id;
+            connections = new LinkedList<LanBridgeConnection>();
+
+            sockets = new LinkedList<Socket>();
+        }
+
+        public void AddConnection(LanBridgeConnection conn)
+        {
+            if (conn.Lan.Id != this.Id)
+                throw new ArgumentException("This connection is not about this LAN.");
+
+            connections.AddLast(conn);
+        }
+
+        private void SetPort(int portNumber)
+        {
+            IPAddress ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            lanSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            lanSocket.Bind(new IPEndPoint(ip, portNumber));
+            lanSocket.Listen(10);
+        }
+
+        private void ListenConnection()
+        {
+            while (lanSocket.Connected)
+            {
+                Socket nextSocket = lanSocket.Accept();
+
+            }
+        }
+
+        //public void AddConnection
+
+        public void Transmit(ConfigurationMessage message)
+        {
+
+        }
+    }
+}
