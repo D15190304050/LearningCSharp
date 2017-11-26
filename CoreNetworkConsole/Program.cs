37e2eb22fd20995072d3a2a327f8fb85fa727223
鲍方舟 <shakuganno00@163.com>
2017-11-26 14:47:51 +0800

    Last updated : 2017.11.26 14:47

--- CoreNetworkConsole/Program.cs
+++ CoreNetworkConsole/Program.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Text;
-using CoreNetworkConsole.DistributedSpanningTrees;
 
 namespace CoreNetworkConsole
 {
@@ -11,12 +10,7 @@ namespace CoreNetworkConsole
     {
         public static int Main(string[] args)
         {
-            const string tinyGraph01 = @"DistributedSpanningTrees\tinyGraph01.txt";
-            const string tinyGraph02 = @"DistributedSpanningTrees\tinyGraph02.txt";
-            const string tinyGraph03 = @"DistributedSpanningTrees\tinyGraph03.txt";
-
-            //SpanningTreeUnitTest.AsStandaloneTest(tinyGraph02);
-            SpanningTreeUnitTest.AsMultiThreadTest(tinyGraph02);
+            
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to continue...");
