eb6fab4573313e69d0021dbb21a48a5964c8d859
鲍方舟 <shakuganno00@163.com>
2018-04-11 21:10:56 +0800

    Last updated: 2018.04.11

--- AlgorithmDesigns/KnapsackProblem.cs
+++ AlgorithmDesigns/KnapsackProblem.cs
@@ -40,12 +40,42 @@ namespace AlgorithmDesigns
             double remainingCapacity = totalCapacity;
             while ((nextItem > 0) && (remainingCapacity > 0))
             {
-                if ((traces[(int)remainingCapacity, nextItem] != traces[(int)remainingCapacity, nextItem - 1]))
+                if (traces[(int)remainingCapacity, nextItem] != traces[(int)remainingCapacity, nextItem - 1])
                 {
-                    selectedItems[nextItem] = true;
+                    selectedItems[nextItem - 1] = true;
                     remainingCapacity -= weights[nextItem - 1];
                 }
                 nextItem--;
+            }
+
+            return traces[totalCapacity, numItems];
+        }
+
+        public static double SingleItem(int totalCapacity, double[] weights, double[] values)
+        {
+            if (totalCapacity <= 0)
+                throw new ArgumentException("The capacity is negative.");
+            else if ((weights == null) || (values == null))
+                throw new ArgumentException("Input array is null.");
+            else if (weights.Length != values.Length)
+                throw new ArgumentException("Length of weights and values are not equal.");
+
+            int numItems = values.Length;
+
+            double[,] traces = new double[totalCapacity + 1, numItems + 1];
+
+            for (int i = 0; i <= totalCapacity; i++)
+                traces[i, 0] = 0;
+
+            for (int item = 1; item <= numItems; item++)
+            {
+                for (int capacity = 0; capacity <= totalCapacity; capacity++)
+                {
+                    if (capacity >= weights[item - 1])
+                        traces[capacity, item] = Math.Max(traces[capacity, item - 1], traces[capacity - (int)(weights[item - 1]), item - 1] + values[item - 1]);
+                    else
+                        traces[capacity, item] = traces[capacity, item - 1];
+                }
             }
 
             return traces[totalCapacity, numItems];
--- AlgorithmDesigns/Program.cs
+++ AlgorithmDesigns/Program.cs
@@ -22,19 +22,28 @@ namespace AlgorithmDesigns
             //UnitTest.NumberOfInversionsUnitTest();
             //UnitTest.RoundRobinFunctionalityTest(16);
 
-            // O(k,j) = max(O(k,j-1), vj + O(k-wj,j-1)) if wj ≤ k.
-            double[] values = { 5, 6, 3 };
-            double[] weights = { 4, 5, 2 };
-            int totalCapacity = 9;
+            // R[i] = Max(R[i - 1], R[j] + p[i]).
+            double[] distances = { 0, 1, 3.14, 5.6, 9.4, 10.6, 11.3 };
+            double[] profits = { 0, 3, 1, 5, 2, 6, 3 };
+            double minDistance = 4;
 
-            double maxValue = KnapsackProblem.SingleItem(totalCapacity, weights, values, out bool[] selectedItems);
-            Console.WriteLine(maxValue);
-            for (int i = 0; i < selectedItems.Length; i++)
+            double[] traces = new double[distances.Length];
+            traces[1] = profits[0];
+            for (int i = 2; i < traces.Length; i++)
             {
-                if (selectedItems[i])
-                    Console.Write(i + " ");
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (distances[i] - distances[j] >= minDistance)
+                    {
+                        double possibleProfit = Math.Max(traces[i - 1], traces[j] + profits[i]);
+                        traces[i] = Math.Max(traces[i], possibleProfit);
+                    }
+                    else
+                        traces[i] = Math.Max(traces[i - 1], traces[i]);
+                }
             }
-            Console.WriteLine();
+
+            Console.WriteLine(traces[distances.Length - 1]);
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to continue...");
