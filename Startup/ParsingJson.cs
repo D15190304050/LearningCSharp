68d917448fc8bdb658b19160d51a606ace4bdc93
鲍方舟 <shakuganno00@163.com>
2019-01-31 20:44:45 +0800

    Add JSON array parsing demo.

--- Startup/ParsingJson.cs
+++ Startup/ParsingJson.cs
@@ -29,6 +29,36 @@ namespace Startup
             // Query value associated with the specified key in the JSON object.
             Console.WriteLine(jsonObject["channel"]["link"]);
 
+            // Test for JSON array.
+            JObject account1 = JObject.FromObject(new
+            {
+                Name = "Nathan",
+                Age = 12
+            });
+            JObject account2 = JObject.FromObject(new
+            {
+                Name = "Chris",
+                Age = 32
+            });
+            JObject account3 = JObject.FromObject(new
+            {
+                Name = "Leon",
+                Age = 24
+            });
+
+            JArray array = new JArray();
+            array.Add(account1);
+            array.Add(account2);
+            array.Add(account3);
+            JObject accounts = JObject.FromObject(new
+            {
+                Type = "Accounts",
+                Body = array
+            });
+
+            Console.WriteLine(accounts);
+            Console.WriteLine(accounts["Body"][1]["Age"]);
+
             // Convert it to bytes and convert back to string.
             byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonObject.ToString());
             string jsonText = Encoding.UTF8.GetString(jsonBytes);
