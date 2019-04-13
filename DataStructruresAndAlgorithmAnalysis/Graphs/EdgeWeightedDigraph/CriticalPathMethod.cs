2c36a0487929102c9ddcec71df1dd1cdc42183ff
鲍方舟 <shakuganno00@163.com>
2019-04-13 20:12:06 +0800

    Fix some typos.

--- DataStructruresAndAlgorithmAnalysis/Graphs/EdgeWeightedDigraph/CriticalPathMethod.cs
+++ DataStructruresAndAlgorithmAnalysis/Graphs/EdgeWeightedDigraph/CriticalPathMethod.cs
@@ -21,29 +21,29 @@ namespace PersonalDataStructuresAndAlgorithm.Graphs.EdgeWeightedDirectedGraph
             string[] lines = System.IO.File.ReadAllLines(fullFileName);
 
             // Number of jobs.
-            int numberOfJbos = int.Parse(lines[0]);
+            int numberOfJobs = int.Parse(lines[0]);
 
             // Source and sink.
-            int source = numberOfJbos * 2;
-            int sink = numberOfJbos * 2 + 1;
+            int source = numberOfJobs * 2;
+            int sink = numberOfJobs * 2 + 1;
 
             // Build network.
-            EdgeWeightedDigraph G = new EdgeWeightedDigraph(numberOfJbos * 2 + 2);
-            for (int i = 0; i < numberOfJbos; i++)
+            EdgeWeightedDigraph G = new EdgeWeightedDigraph(numberOfJobs * 2 + 2);
+            for (int i = 0; i < numberOfJobs; i++)
             {
                 // Split the schedule info into individual words.
                 string[] line = System.Text.RegularExpressions.Regex.Split(lines[i + 1], "\\s+");
 
                 double duration = double.Parse(line[0]);
                 G.AddEdge(new DirectedEdge(source, i, 0.0));
-                G.AddEdge(new DirectedEdge(i + numberOfJbos, sink, 0.0));
-                G.AddEdge(new DirectedEdge(i, i + numberOfJbos, duration));
+                G.AddEdge(new DirectedEdge(i + numberOfJobs, sink, 0.0));
+                G.AddEdge(new DirectedEdge(i, i + numberOfJobs, duration));
 
                 // Precedence constrains.
                 for (int j = 1; j < line.Length; j++)
                 {
                     int precedence = int.Parse(line[j]);
-                    G.AddEdge(new DirectedEdge(i + numberOfJbos, precedence, 0.0));
+                    G.AddEdge(new DirectedEdge(i + numberOfJobs, precedence, 0.0));
                 }
             }
 
@@ -53,8 +53,8 @@ namespace PersonalDataStructuresAndAlgorithm.Graphs.EdgeWeightedDirectedGraph
             // Print results.
             Console.WriteLine("  job  start  finish");
             Console.WriteLine("---------------------");
-            for (int i = 0; i < numberOfJbos; i++)
-                Console.WriteLine("{0,4} {1,7:F1} {2,7:F1}", i, lp.DistanceTo(i), lp.DistanceTo(i + numberOfJbos));
+            for (int i = 0; i < numberOfJobs; i++)
+                Console.WriteLine("{0,4} {1,7:F1} {2,7:F1}", i, lp.DistanceTo(i), lp.DistanceTo(i + numberOfJobs));
             Console.WriteLine("Finish time: {0,7:F1}", lp.DistanceTo(sink));
         }
     }
