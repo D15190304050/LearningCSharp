c05c21005f484d69c8c5bbcdf73cf452a7280e57
鲍方舟 <shakuganno00@163.com>
2019-07-11 19:14:17 +0800

    Keep coding...

--- Startup/Program.cs
+++ Startup/Program.cs
@@ -12,45 +12,47 @@ namespace Startup
         {
             //MySqlConnectionTest.ConnectionTest();
 
-            double[,] parameters = new double[6, 3];
+            //double[,] parameters = new double[6, 3];
 
-            parameters[0, 0] = 0;
-            parameters[0, 1] = 0;
-            parameters[0, 2] = 0;
+            //parameters[0, 0] = 0;
+            //parameters[0, 1] = 0;
+            //parameters[0, 2] = 0;
 
-            parameters[1, 0] = 0;
-            parameters[1, 1] = 0;
-            parameters[1, 2] = 1;
+            //parameters[1, 0] = 0;
+            //parameters[1, 1] = 0;
+            //parameters[1, 2] = 1;
 
-            parameters[2, 0] = 0;
-            parameters[2, 1] = 4;
-            parameters[2, 2] = 5;
+            //parameters[2, 0] = 0;
+            //parameters[2, 1] = 4;
+            //parameters[2, 2] = 5;
 
-            parameters[3, 0] = 1;
-            parameters[3, 1] = 2;
-            parameters[3, 2] = 1;
+            //parameters[3, 0] = 1;
+            //parameters[3, 1] = 2;
+            //parameters[3, 2] = 1;
 
-            parameters[4, 0] = 1;
-            parameters[4, 1] = 2;
-            parameters[4, 2] = 3;
+            //parameters[4, 0] = 1;
+            //parameters[4, 1] = 2;
+            //parameters[4, 2] = 3;
 
-            parameters[5, 0] = 1;
-            parameters[5, 1] = 3;
-            parameters[5, 2] = 2;
+            //parameters[5, 0] = 1;
+            //parameters[5, 1] = 3;
+            //parameters[5, 2] = 2;
 
-            QuadraticEquation qe;
-            for (int i = 0; i < parameters.GetLength(0); i++)
-            {
-                qe = new QuadraticEquation(parameters[i, 0], parameters[i, 1], parameters[i, 2]);
-                if (!qe.IsSolvable)
-                    Console.WriteLine("({0}) * x^2 + ({1}) * x + ({2}) = 0 has no solution\n", parameters[i, 0], parameters[i, 1], parameters[i, 2]);
-                else
-                {
-                    Console.WriteLine("Solution for ({0}) * x^2 + ({1}) * x + ({2}) = 0 is", parameters[i, 0], parameters[i, 1], parameters[i, 2]);
-                    Console.WriteLine("Root1 = {0}", qe.Root1);
-                    Console.WriteLine("Root2 = {0}\n", qe.Root2);
-                }
-            }
+            //QuadraticEquation qe;
+            //for (int i = 0; i < parameters.GetLength(0); i++)
+            //{
+            //    qe = new QuadraticEquation(parameters[i, 0], parameters[i, 1], parameters[i, 2]);
+            //    if (!qe.IsSolvable)
+            //        Console.WriteLine("({0}) * x^2 + ({1}) * x + ({2}) = 0 has no solution\n", parameters[i, 0], parameters[i, 1], parameters[i, 2]);
+            //    else
+            //    {
+            //        Console.WriteLine("Solution for ({0}) * x^2 + ({1}) * x + ({2}) = 0 is", parameters[i, 0], parameters[i, 1], parameters[i, 2]);
+            //        Console.WriteLine("Root1 = {0}", qe.Root1);
+            //        Console.WriteLine("Root2 = {0}\n", qe.Root2);
+            //    }
+            //}
+
+            TaskDemos.InstantiateTasks();
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to continue...");
---
+++ Startup/TaskDemos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Startup
+{
+    public static class TaskDemos
+    {
+        public static void InstantiateTasks()
+        {
+            Action<object> action = (object o) =>
+                Console.WriteLine($"Task={Task.CurrentId}, object={o}, Thread={Thread.CurrentThread.ManagedThreadId}");
+
+            // Create a task but do not start it.
+            Task t1 = new Task(action, "alpha");
+
+            // Construct a started task.
+            Task t2 = Task.Factory.StartNew(action, "beta");
+            // Block the main thread to demonstrate that t2 is executing.
+            t2.Wait();
+
+            // Launch t1.
+            t1.Start();
+            Console.WriteLine($"t1 has been launched. (Main Thread={Thread.CurrentThread.ManagedThreadId})");
+            // Wait for the task to finish.
+            t1.Wait();
+
+            // Construct a started task using Task.Run.
+            Task t3 = Task.Run(() =>
+                Console.WriteLine($"Task={Task.CurrentId}, object=delta, Thread={Thread.CurrentThread.ManagedThreadId}"));
+            // Wait for the task to finish.
+            t3.Wait();
+
+            // Construct an unstarted task.
+            Task t4 = new Task(action, "gamma");
+            // Run it synchronously.
+            t4.RunSynchronously();
+            // Although the task was run synchronously, it is a good practice to wait for it in the event exceptions were thrown by the task.
+            t4.Wait();
+        }
+    }
+}
