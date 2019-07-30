4e456d0d95c2eb2088a875d3912697df99a5ebf0
D15190304050 <15190304050@163.com>
2019-07-30 23:00:15 +0800

    Fix some typos.

--- DataTools/Program.cs
+++ DataTools/Program.cs
@@ -11,28 +11,11 @@ namespace DataTools
 {
     public class Program
     {
-        static int Main(string[] args)
+        public static int Main(string[] args)
         {
-            //Graphs.UnitTest.FordFulkersonUnitTest();
-            //Graphs.UnitTest.ArbitrageUnitTest();
-            string graphFile = @"F:\DinoStark\Temp\graph.txt";
-            //string[] text = File.ReadAllLines(graphFile);
-            //for (int i = 0; i < text.Length; i++)
-            //{
-            //    string[] line = text[i].Split(' ');
-            //    char left = line[0][0];
-            //    char right = line[1][0];
-
-            //    text[i] = ((int)(left - 'A') + " " + (int)(right - 'A'));
-            //}
-            //File.WriteAllLines(graphFile, text);
-            Digraph G = new Digraph(graphFile);
-            G = G.Reverse();
-            DepthFirstOrder order = new DepthFirstOrder(G);
-            Console.WriteLine(order.ReversePostOrder != null);
-            foreach (int i in order.ReversePostOrder)
-                Console.Write((char)('A' + i) + " ");
-            Console.WriteLine();
+            //DataTools.Collections.UnitTest.LinkedListUnitTest();
+            //DataTools.Collections.UnitTest.QueueUnitTest();
+            DataTools.Collections.UnitTest.StackUnitTest();
 
             // Keep the console open during debug mode.
             Console.WriteLine("Press any key to continue...");
